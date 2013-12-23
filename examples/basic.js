818e8cc554412d9e5ed053db831f15bc71a7eaaa
Brandon Tilley <brandon@brandontilley.com>
2013-12-23 15:02:42 -0800

    Update site

--- examples/basic.js
+++ examples/basic.js
@@ -6,9 +6,10 @@
   planet.loadPlugin(planetaryjs.plugins.earth({
     topojson: { file: '/world-110m.json' }
   }));
-  // Scale the planet's radius to half the canvas' size...
-  planet.projection.scale(canvas.width / 2);
-  // ...and move the planet to the center of the canvas.
-  planet.projection.translate([canvas.width / 2, canvas.height / 2]);
+  // Scale the planet's radius to half the canvas' size
+  // and move it to the center of the canvas.
+  planet.projection
+    .scale(canvas.width / 2)
+    .translate([canvas.width / 2, canvas.height / 2]);
   planet.draw(canvas);
 })();
